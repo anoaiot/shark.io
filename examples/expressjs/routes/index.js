8ae8ba61d09a355f8e5b4b62747f1f8f4a5f8932
anak10thn <anak10thn@gmail.com>
2015-04-15 04:09:43 +0700

    ADD: expressjs example

---
+++ examples/expressjs/routes/index.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+
+var shark = require('shark.io');
+shark.init('127.0.0.1:6969');
+var api = shark.api;
+var apis;
+api.on('open',function(event){
+    apis = event.serial;
+})
+/* GET home page. */
+router.get('/', function(req, res) {
+  apis.info(function(data){
+	res.send(data);
+  });
+});
+
+module.exports = router;
