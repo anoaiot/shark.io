35e269e56d72c186f4106060997b98ec92d8173d
anak10thn <anak10thn@gmail.com>
2015-04-21 04:48:53 +0700

    Bump version to 0.1.7

---
+++ examples/gpio_read.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var shark = require('shark.io');
+var sleep = require('sleep');
+
+shark.init('127.0.0.1:6969');
+var loop = shark.loop;
+var gpio;
+shark.setup.on('open',function(event){
+    gpio = event.gpio;
+    gpio.set(18);
+    gpio.mode("in");
+    gpio.read(18,function(out){
+       out.stream.connect(function(data){
+          if(data == 1){
+             console.log("ON")
+          }
+       });
+    });
+});
